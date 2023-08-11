e71826110351525a2ba4c733246ef180a2dcac8b
zloibala <boronova25@gmail.com>
2023-08-10 20:11:35 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import NavBar from "./components/navBar/NavBar";
 import { Routes, Route } from "react-router-dom";
 import BecomeDriver from "./components/pages/BecomeDriver";
+import Home from "./components/pages/home";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/becomeDriver" element={<BecomeDriver />} />
+        <Route path="home" element={<Home />} />
       </Routes>
     </div>
   );
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navBar.style.css";
 
 const NavBar = () => {
+  const [contact, setContact] = useState();
+
+  const onClickContact = () => {
+    console.log("cliked");
+  };
   return (
     <div className="nav--bar-container">
       <nav className="nav--bar">
         <a href="/home">
-          <h3> LOGIRECRUIT </h3>
+          <h3 className="main--logo"> LOGIRECRUIT </h3>
         </a>
         <ul>
           <li>
@@ -23,12 +28,24 @@ const NavBar = () => {
             <a href="/findDriver"> Find a Driver</a>
           </li>
         </ul>
+
         <ul>
           <li>
-            <a href="/contact"> Contact</a>
+            <button className=" btn btn--contact" onClick={onClickContact}>
+              Contact
+            </button>
           </li>
         </ul>
       </nav>
+      <section className="home--section">
+        <h1> Fueling Futures, Hiring the Best Drivers</h1>
+        <p>
+          Discover a road to success like never before - join our team of
+          skilled drivers where every mile driven is a step towards a rewarding
+          career in the dynamic world of logistics
+        </p>
+        <button className=" btn btn--apply"> Apply </button>
+      </section>
     </div>
   );
 };
