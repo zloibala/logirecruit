ef41d328fdef985daab06a519dcaa9b6e938e666
zloibala <boronova25@gmail.com>
2023-08-23 18:42:52 -0500

    small changes

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+// import { Routes, Route } from "react-router";
+import AboutUs from "./AboutUs";
+import "./home.style.css";
 
 const Home = () => {
   return (
-    <div>
-      <section className="home--section">
-        <h1> Fueling Futures, Hiring the Best Drivers</h1>
-        <p>
-          Discover a road to success like never before - join our team of
-          skilled drivers where every mile driven is a step towards a rewarding
-          career in the dynamic world of logistics
-        </p>
-        <button className=" btn btn--apply"> Apply </button>
-        <image src="https://d2uhsaoc6ysewq.cloudfront.net/57185/Refrigerated-Trailers-Great-Dane-53FT-REEFER---FLAT-FLOOR--EXCELLENT-SHAPE-32889790.jpg" />
-      </section>
+    <div className="landing--page">
+      <h1> Fueling Futures, Hiring the Best Drivers</h1>
+      <p>
+        Discover a road to success like never before - join our team of skilled
+        drivers where every mile driven is a step towards a rewarding career in
+        the dynamic world of logistics
+      </p>
+      <button className=" btn btn--apply"> Apply </button>
+
+      {/* <AboutUs /> */}
     </div>
   );
 };
